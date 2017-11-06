0a3665efbf78cd3749379eea976b701394981bda
Vladimir Dejanovic <ed.wong.iv@gmail.com>
2017-11-06 21:18:49 +0100

    init code

---
+++ src/main/java/xyz/itshark/restdemo/simplespringbootrestmongoconftalkdemo/SimpleSpringbootRestMongoConftalkDemoApplication.java
@@ -0,0 +1,12 @@
+package xyz.itshark.restdemo.simplespringbootrestmongoconftalkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootRestMongoConftalkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringbootRestMongoConftalkDemoApplication.class, args);
+	}
+}
